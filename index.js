fb08a4fe5f7f1905850639ccfddc6e44843bb319
Emilio <marqetm@gmail.com>
2022-06-12 04:54:22 -0500

    commit tree 017015cc2c1645a7c169a8ceb27d04a8fa228b23

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function(bookTitlesJson) {
+    renderBooks(bookTitlesJson)
+  })
 }
 
-function renderBooks(books) {
+function renderBooks(bookTitlesJson) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  bookTitlesJson.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML =`${book.name}`;
     main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
